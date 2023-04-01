bfb9015a3969a4f8b817df56ec15f4a72dc4a7e8
AdarshVMore <skullcrushermore@gmail.com>
2023-04-01 23:20:43 +0530

    map bug solved

--- src/pages/home/components/mainContainer/home/Home.jsx
+++ src/pages/home/components/mainContainer/home/Home.jsx
@@ -5,6 +5,7 @@ import { PAddressRef } from "../../topNav/TopNav";
 function Home({ contract, account }) {
   const [isDoc, setIsDoc] = useState(false);
   const [patientInfo, setPatientInfo] = useState([]);
+  const [click, setClick] = useState(false);
 
   let addr;
   useEffect(() => {
@@ -34,32 +35,31 @@ function Home({ contract, account }) {
     const Info = await contract.get_patient(addr);
     console.log(Info);
     setPatientInfo(Info);
-    console.log(
-      patientInfo,
-      patientInfo[2][0].patientAddress
-      // patientInfo[2].length
-    );
+    setClick(true);
+    console.log(patientInfo, patientInfo[2][0].patientAddress, click);
   };
   return (
     <div>
       <button onClick={getPatient}>Get Records</button>
       <div>
-        <p>Patient Name: {patientInfo[0]}</p>
         <div className="records">
           <div className="article">
-            {patientInfo[2].map((record, i) => (
-              <div key={i} className="article">
-                <p>Patient Address: {record.patientAddress}</p>
-                <p>Doctor Name: {record.doctorName}</p>
-                <p>Symptoms: {record.symptoms}</p>
-                <p>Diagnosis: {record.diagnosis}</p>
-                <p>Reports: {record.report}</p>
-                <p>Treatment: {record.treatment}</p>
-                <p>Medication: {record.Medication}</p>
-                <p>Details: {record.details}</p>
-                <p>Bills: {record.bill}</p>
-              </div>
-            ))}
+            {click
+              ? patientInfo[2].map((record, i) => (
+                  <div key={i} className="article">
+                    <p>Patient Name: {patientInfo[0]}</p>
+                    <p>Patient Address: {record.patientAddress}</p>
+                    <p>Doctor Name: {record.doctorName}</p>
+                    <p>Symptoms: {record.symptoms}</p>
+                    <p>Diagnosis: {record.diagnosis}</p>
+                    <p>Reports: {record.report}</p>
+                    <p>Treatment: {record.treatment}</p>
+                    <p>Medication: {record.Medication}</p>
+                    <p>Details: {record.details}</p>
+                    <p>Bills: {record.bill}</p>
+                  </div>
+                ))
+              : ""}
           </div>
         </div>
       </div>
